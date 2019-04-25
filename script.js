0e3b4c76e919b815d1dba0772673b8ec360bcd83
Mateusz <46346197+MatiMatik97@users.noreply.github.com>
2019-04-25 23:48:14 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,97 @@
+var divOne = document.getElementById("divOne");
+
+//////////////////////////////////////////////////////
+
+var arr = [1, 2, 4, 2, 1];
+
+function addNum() {
+	if(document.getElementById("num").value != "") {
+		arr.push(document.getElementById("num").value);
+		val = document.getElementById("num").value;
+		
+		divOne.innerHTML = '';
+		show();
+	}
+}
+
+function findLeast(arr) {
+	var times = [];
+	
+	for(i in arr) {
+		for(j in times) {
+			if(arr[i] == times[j].Value) {
+				times[j].Times += 1;
+			} else {
+				var data = {
+					"Value": arr[i],
+					"Times": 1
+				};
+				
+				times.push(data);
+			}
+		}
+		
+		if(times.length == 0) {
+			var data = {
+				"Value": arr[i],
+				"Times": 1
+			};
+			
+			times.push(data);
+		}
+	}
+	
+	var maxTimes = times[0].Times;
+	var valueWithMaxTimes = times[0].Value;
+	
+	for(i in times) {
+		if(times[i].Times > maxTimes) {
+			maxTimes = times[i].Times;
+			valueWithMaxTimes = times[i].Value;
+		}
+	}
+	
+	return valueWithMaxTimes;
+}
+
+var val = 1;
+
+function show() {
+	divOne.innerHTML += 'Returns the value that occurs the maximum nubmer of times</br></br>Array: ';
+
+	for(i in arr) {
+		divOne.innerHTML += arr[i] + ' ';
+	}
+	
+	var maxTimes = findLeast(arr);
+	
+	divOne.innerHTML +=	'</br></br>Add number to the array: ';
+	divOne.innerHTML += '<input type="number" name="num" id="num" value="'+val+'"/>';
+	divOne.innerHTML += '<button id="addButton" onclick="addNum()">Add</button>';
+	divOne.innerHTML += '</br></br>Value that occurs maximum nubmer of times: ';
+	divOne.innerHTML += maxTimes;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//////////////////////////////////////////////////////
